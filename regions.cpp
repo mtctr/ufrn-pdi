73802ac36783a80bb0c32318831c6b670d300927
Mateus Targino <mateus@debian.com>
2018-03-07 21:49:08 -0300

    exercício 1: inverter cores

---
+++ regions.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main (int, char**){
+  Mat image;
+  Vec3b val;
+  int p1x, p1y, p2x, p2y;
+
+  image =  imread("saladca.png", CV_LOAD_IMAGE_COLOR);
+
+  if(!image.data){
+    cout << "imagem não encontrada" << endl;
+  } else{
+    resize(image, image, Size(960,540));
+  }
+
+  namedWindow("janela",WINDOW_AUTOSIZE);
+
+  cout<<"Escolha um ponto P1(x) entre 0 e "<<image.cols<<":" << endl;
+  cin>>p1x;
+  cout<<"Escolha um ponto P2(x) entre 0 e "<<image.cols<<":" << endl;
+  cin>>p2x;
+
+  cout<<"Escolha um ponto P1(y) entre 0 e "<<image.rows<<":" << endl;
+  cin>>p1y;
+  cout<<"Escolha um ponto P2(y) entre 0 e "<<image.rows<<":" << endl;
+  cin>>p2y;
+
+  if(p1x > p2x){
+    int aux;
+    aux = p1x;
+    p1x = p2x;
+    p2x = aux;
+  }
+  if(p1y > p2y){
+    int aux;
+    aux = p1y;
+    p1y = p2y;
+    p2y = aux;
+  }
+
+  for (int i = p1y; i < p2y; i++){
+    for(int j = p1x; j< p2x; j++){
+      val = image.at<Vec3b>(i,j);
+
+      val[0] = 255 - val[0];
+      val[1] = 255 - val[2];
+      val[2] = 255 - val[2];
+      image.at<Vec3b>(i,j)= val;
+    }
+  }
+
+
+
+
+  imshow("janela", image);
+  waitKey();
+  return 0;
+}
